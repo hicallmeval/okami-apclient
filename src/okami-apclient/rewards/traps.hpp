2719d4ee7553fc9a70060b5e2360b67a5b6132e8
Val <gavin.kiener@icloud.com>
2026-08-01 12:48:34 -0700

    format

--- src/okami-apclient/rewards/traps.hpp
+++ src/okami-apclient/rewards/traps.hpp
@@ -14,8 +14,8 @@
 namespace rewards::traps
 {
 
-inline constexpr int64_t kTrapDamage   = 0x400; // Evil Charm:    deal 50% max HP (never lethal)
-inline constexpr int64_t kTrapInkLoss  = 0x401; // Dry Inkwell:   drain all current ink
+inline constexpr int64_t kTrapDamage = 0x400;   // Evil Charm:    deal 50% max HP (never lethal)
+inline constexpr int64_t kTrapInkLoss = 0x401;  // Dry Inkwell:   drain all current ink
 inline constexpr int64_t kTrapFoodLoss = 0x402; // Hungry Spirit: drain the Astral Pouch (food)
 
 /**
